b8f4c14358f09e4c275ccbf4a82f090791db7636
Okata Miracle <okatamiracle.dev@gmail.com>
2025-10-31 03:53:56 +0100

    style(Home): adjust padding and spacing in Home component

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -4,7 +4,7 @@ import About from "./About/page";
 
 const Home = () => {
   return (
-    <div className="pt-20 md:pt-30">
+    <div className="pt-20 pb-20 md:pt-30">
       <div className="flex flex-col space-y-10 md:space-y-15 lg:space-y-25">
         <h1
           className={`${ala.className} text-3xl md:text-5xl lg:text-7xl lg:text-center font-bold text-gray-700 underline underline-offset-2 md:underline-offset-10`}
