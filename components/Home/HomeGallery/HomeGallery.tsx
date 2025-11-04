9183f35bac40ff131ad850f3f02e293d00a21b8c
Okata Miracle <okatamiracle.dev@gmail.com>
2025-11-04 11:10:24 +0100

    fix(HomeGallery): adjust collage positioning for better layout
    Update top positions of collages #1 and #4 to improve visual balance and spacing in the gallery component

--- components/Home/HomeGallery/HomeGallery.tsx
+++ components/Home/HomeGallery/HomeGallery.tsx
@@ -47,7 +47,7 @@ const HomeGallery = () => {
           height={2000}
           className="w-full h-full object-contain"
         />
-        <div className="absolute right-20 md:right-60 top-23 md:top-26 active:rotate-12 transition-all duration-500 rotate-6">
+        <div className="absolute right-20 md:right-60 top-43 md:top-26 active:rotate-12 transition-all duration-500 rotate-6">
           <Collage title="#1" image="/hair/hair-1.jpg" />
         </div>
         <div className="absolute left-10 md:left-55 top-63 md:top-66 active:-rotate-12 transition-all duration-500 rotate-20">
@@ -56,7 +56,7 @@ const HomeGallery = () => {
         <div className="absolute right-10 md:right-40 top-93 md:top-106 active:rotate-6 transition-all duration-500 rotate-20">
           <Collage title="#3" image="/hair/hair-3.jpg" />
         </div>
-        <div className="absolute left-50 md:left-95 top-110 md:top-130 active:-rotate-12 transition-all duration-500 rotate-6">
+        <div className="absolute left-50 md:left-95 top-135 md:top-130 active:-rotate-12 transition-all duration-500 rotate-6">
           <Collage title="#4" image="/hair/hair-4.jpg" />
         </div>
         <div className="absolute right-20 md:right-65 top-170 md:top-200 active:rotate-6 transition-all duration-500 rotate-16">
