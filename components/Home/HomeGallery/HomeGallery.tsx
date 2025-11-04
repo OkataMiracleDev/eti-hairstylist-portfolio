db71571df344637fafa5a79c977ae9563f171bc7
Okata Miracle <okatamiracle.dev@gmail.com>
2025-11-04 11:12:16 +0100

    style(HomeGallery): adjust margin classes for better spacing

--- components/Home/HomeGallery/HomeGallery.tsx
+++ components/Home/HomeGallery/HomeGallery.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const HomeGallery = () => {
   return (
-    <div className="mt-20 md:mt-30 lg:-mt-180 flex flex-col items-center">
+    <div className="md:mt-10 lg:-mt-180 flex flex-col items-center">
       <div className="w-[120%] h-300 relative hidden lg:flex flex-col ">
         <Image
           src={"/svg/line-pc.svg"}
