73371fb174c3100f3ac62620a11508d42b8fc5ac
Okata Miracle <okatamiracle.dev@gmail.com>
2025-11-04 21:33:55 +0100

    feat(Gallery): improve gallery layout and add hover effects
    - Replace placeholder content with actual images
    - Add hover and active scale effects to gallery items
    - Adjust grid gaps and margins for better spacing
    - Ensure consistent overflow handling and positioning

--- app/Gallery/page.tsx
+++ app/Gallery/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Gallery = () => {
   return (
     <div className="relative w-full overflow-y-auto overflow-x-hidden px-5 md:px-20">
-      <div className="pt-30 w-full flex flex-col md:gap-5 gap-10 justify-center items-center">
+      <div className="pt-30 w-full flex flex-col gap-5 justify-center items-center">
         <h1 className="text-xl md:text-3xl lg:text-5xl lg:text-center font-bold text-gray-900">
           Gallery
         </h1>
@@ -12,24 +12,88 @@ const Gallery = () => {
           Here you can see a selection of my work.
         </p>
 
-        <div className="w-full grid grid-cols-1 md:grid-cols-3 justify-center gap-1">
-          <div className="bg-black w-full h-80">
+        <div className="md:mt-4 w-full grid grid-cols-1 md:grid-cols-3 justify-center gap-1">
+          <div className="bg-black w-full h-80 overflow-hidden relative">
             <Image
               src="/hair/hair-1.jpg"
               alt="1"
-              width={2000}
-              height={2000}
-              className="w-full h-full object-cover"
+              width={1000}
+              height={1000}
+              className="w-full h-full object-cover block active:scale-120 hover:scale-120 transition-all duration-500"
             />
+            <div className="w-full h-full absolute"></div>
           </div>
-          <div className="bg-black w-full h-110">1</div>
-          <div className="bg-black w-full h-65">1</div>
-          <div className="bg-black md:-mt-30 w-full h-80">1</div>
-          <div className="bg-black w-full h-70">1</div>
-          <div className="bg-black md:-mt-45 w-full h-100">1</div>
-          <div className="bg-black md:hidden w-full h-65">1</div>
+          <div className="bg-black w-full h-110 overflow-hidden relative">
+            <Image
+              src="/hair/hair-3.jpg"
+              alt="2"
+              width={1000}
+              height={1000}
+              className="w-full h-full object-cover object-bottom block active:scale-120 hover:scale-120 transition-all duration-500"
+            />
+            <div className="w-full h-full absolute"></div>
+          </div>
+          <div className="bg-black w-full h-65 overflow-hidden relative">
+            <Image
+              src="/hair/hair-2.jpg"
+              alt="3"
+              width={1000}
+              height={1000}
+              className="w-full h-full object-cover object-center lg:object-top block active:scale-120 hover:scale-120 transition-all duration-500"
+            />
+            <div className="w-full h-full absolute"></div>
+          </div>
+          <div className="bg-black md:-mt-30 w-full h-80 overflow-hidden relative">
+            <Image
+              src="/hair/hair-4.jpg"
+              alt="4"
+              width={1000}
+              height={1000}
+              className="w-full h-full object-cover object-center block active:scale-120 hover:scale-120 transition-all duration-500"
+            />
+            <div className="w-full h-full absolute"></div>
+          </div>
+          <div className="bg-black w-full h-70 overflow-hidden relative">
+            <Image
+              src="/hair/hair-7.jpg"
+              alt="5"
+              width={1000}
+              height={1000}
+              className="w-full h-full object-cover object-center block active:scale-120 hover:scale-120 transition-all duration-500"
+            />
+            <div className="w-full h-full absolute"></div>
+          </div>
+          <div className="bg-black md:-mt-45 w-full h-100 overflow-hidden relative">
+            <Image
+              src="/hair/hair-6.jpg"
+              alt="6"
+              width={1000}
+              height={1000}
+              className="w-full h-full object-cover object-center block active:scale-120 hover:scale-120 transition-all duration-500"
+            />
+            <div className="w-full h-full absolute"></div>
+          </div>
+          <div className="bg-black md:hidden w-full h-65 overflow-hidden relative">
+            <Image
+              src="/hair/hair-8.jpg"
+              alt="8"
+              width={1000}
+              height={1000}
+              className="w-full h-full object-cover object-top block active:scale-120 hover:scale-120 transition-all duration-500"
+            />
+            <div className="w-full h-full absolute"></div>
+          </div>
         </div>
-        <div className="bg-black hidden md:flex md:-mt-4 w-full h-80">1</div>
+        <div className="bg-black hidden md:flex md:-mt-4 w-full h-80 overflow-hidden relative">
+          <Image
+            src="/hair/hair-8.jpg"
+            alt="8"
+            width={1000}
+            height={1000}
+            className="w-full h-full object-cover object-top block active:scale-120 hover:scale-120 transition-all duration-500"
+          />
+          <div className="w-full h-full absolute"></div>
+        </div>
       </div>
     </div>
   );
